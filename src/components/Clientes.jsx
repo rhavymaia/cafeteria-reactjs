81ba7e0368d40760b1ac6aab95695154b6323ff0
rhavymaia <rhavy.guedes@ifpb.edu.br>
2022-12-16 20:29:27 -0300

    Formulário modal com bootstrap.

--- src/components/Clientes.jsx
+++ src/components/Clientes.jsx
@@ -1,11 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+
 import Table from 'react-bootstrap/Table';
+import Form from 'react-bootstrap/Form';
+import ClienteModal from './ClienteModal';
 
 const Clientes = () => {
   const [nome, setNome] = useState('');
 
   const [clientes, setClientes] = useState([]);
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   useEffect(() => {
     fetch('http://localhost:3030/clientes', { method: 'GET' })
@@ -19,11 +28,11 @@ const Clientes = () => {
       .catch((error) => {});
   }, []);
 
-  const handleChange = (event) => {
+  const handleChangeBuscar = (event) => {
     setNome(event.target.value);
   };
 
-  const handleClick = (event) => {
+  const handleClickBuscar = (event) => {
     fetch(`http://localhost:3030/clientes?q=${nome}`, { method: 'GET' })
       .then((response) => {
         return response.json();
@@ -39,36 +48,59 @@ const Clientes = () => {
 
   return (
     <div>
-      <h1>Clientes</h1>
-      <form>
-        <input type="text" id="" value={nome} onChange={handleChange} />
-        <Button variant="primary" onClick={handleClick}>
-          Buscar
+      <Container>
+        <h1 className="text-center">Clientes</h1>
+        <Button className="my-1 float-end" variant="dark" onClick={handleShow}>
+          +
         </Button>
-      </form>
 
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Nome</th>
-            <th>E-mail</th>
-            <th>Nascimento</th>
-          </tr>
-        </thead>
-        <tbody>
-          {clientes.map((cliente) => {
-            return (
-              <tr key={cliente.id}>
-                <td>{cliente.id}</td>
-                <td>{cliente.nome}</td>
-                <td>{cliente.email}</td>
-                <td>{cliente.nascimento}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
+        <Form>
+          <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Label>Nome</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Digite o nome do cliente"
+              name="nome"
+              controlid="nome"
+              value={nome}
+              onChange={handleChangeBuscar}
+            />
+          </Form.Group>
+
+          <Button
+            className="mb-1"
+            variant="primary"
+            onClick={handleClickBuscar}
+          >
+            Buscar
+          </Button>
+        </Form>
+
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Nome</th>
+              <th>E-mail</th>
+              <th>Nascimento</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientes.map((cliente) => {
+              return (
+                <tr key={cliente.id}>
+                  <td>{cliente.id}</td>
+                  <td>{cliente.nome}</td>
+                  <td>{cliente.email}</td>
+                  <td>{cliente.nascimento}</td>
+                </tr>
+              );
+            }) || ''}
+          </tbody>
+        </Table>
+      </Container>
+
+      <ClienteModal show={show} setShow={setShow} handleClose={handleClose} />
     </div>
   );
 };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { CDBFooter, CDBBox, CDBIcon } from 'cdbreact';
+import Button from 'react-bootstrap/Button';
+
+import { MDBFooter, MDBContainer, MDBIcon, MDBBtn } from 'mdb-react-ui-kit';
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <>
+      <MDBFooter
+        className="text-center text-white"
+        style={{ backgroundColor: '#f1f1f1' }}
+      >
+        <MDBContainer className="pt-4">
+          <section className="mb-4">
+            <MDBBtn
+              rippleColor="dark"
+              color="link"
+              floating
+              size="lg"
+              className="text-dark m-1"
+              href="#!"
+              role="button"
+            >
+              <MDBIcon fab className="fa-instagram" />
+            </MDBBtn>
+          </section>
+        </MDBContainer>
+
+        <div
+          className="text-center text-dark p-3"
+          style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
+        >
+          © 2020 Copyright:
+          <a className="text-dark" href="https://mdbootstrap.com/">
+            MDBootstrap.com
+          </a>
+        </div>
+      </MDBFooter>
+    </>
+  );
 };
 
 export default Footer;
